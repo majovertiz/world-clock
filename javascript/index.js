73ebe16611811d3974e35f96bed153056a66acb2
majovertiz <maria.jose.vertiz@gmail.com>
2023-11-17 19:36:44 -0800

    Added js moment

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,23 @@
+function updateTime() {
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment().tz("Europe/London");
 
+  londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = londonTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let mexicoElement = document.querySelector("#mexico");
+  let mexicoDateElement = mexicoElement.querySelector(".date");
+  let mexicoTimeElement = mexicoElement.querySelector(".time");
+  let mexicoTime = moment().tz("America/Mexico_City");
+
+  mexicoDateElement.innerHTML = mexicoTime.format("MMMM Do YYYY");
+  mexicoTimeElement.innerHTML = mexicoTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
